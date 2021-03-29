bb12c1b6950cf5a2f5f59179cfc94da75d7f4121
Marie G <marie.goeske@yahoo.de>
2021-03-29 16:26:54 +0200

    PPM-Image wird in Main erzeugt

--- PPM.py
+++ PPM.py
@@ -1,6 +1,23 @@
+from array import array
+
+from Color import Color
+
+
 class PPM:
-    def __init__(self, width, height, max_color_value, color_array):
-        self.width = width
-        self.height = height
-        self.max_color = max_color_value
-        self.color_array = color_array
+    width = 256
+    height = 128
+    max_val = 255
+    new_Color = Color(0, 123, 1)
+
+    # PPM Header
+    ppm_header = f'P6 {width} {height} {max_val}\n'
+
+    # PPM image data
+    image = array.array('B', new_Color.__sizeof__() * width * height)
+
+    # Save the PPM image as binary file
+    with open('new_img.ppm', 'wb') as f:
+        f.write(bytearray(ppm_header, 'ascii'))
+        image.tofile(f)
+
+
--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import PPM
+import array
+
+from Color import Color
 
 if __name__ == '__main__':
-    ppm = PPM()
+    width = 256
+    height = 128
+    max_val = 255
+    new_Color = Color(0, 123, 1)
+
+    # PPM Header
+    ppm_header = f'P6 {width} {height} {max_val}\n'
+
+    # PPM image data
+    image = array.array('B', [0, 0, 255] * width * height)
+
+  #  Fill with red the rectangle with origin at (10, 10) and width x height = 50 x 80 pixels
+
+for y in range(10, 90):
+
+    for x in range(10, 60) :
+        index = 3 * (y * width + x)
+        image[index] = 255  # red channel
+        image[index + 1] = 0  # green channel
+        image[index + 2] = 0  # blue channel
+
+
+# Save the PPM image as binary file
+    with open('new_img.ppm', 'wb') as f:
+        f.write(bytearray(ppm_header, 'ascii'))
+        image.tofile(f)
